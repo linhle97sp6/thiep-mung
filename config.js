f9be80cdaf5271e810155428c4d36d25e54e9920
linhle97sp6 <92876893+linhle97sp6@users.noreply.github.com>
2021-10-20 22:58:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Duyên';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng đứa bạn thân';
+const titleCard = 'Tặng bạn thân';
 const contentCard = 'Chúc Duyên 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Duyên trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
